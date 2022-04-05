a6e8ca2b3109174141286ccde5180f3ad0e2ef8f
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2022-04-05 12:12:00 +1200

    Reorganisation of code + view all users

---
+++ database-functions.js
@@ -0,0 +1,39 @@
+// ESTABLISH MONGOOSE CONNECTION
+require('dotenv').config();
+var mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// DATABASE SCHEMAS
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {type: String, required: true}
+}, {versionKey: false})
+
+const exerciseSchema = new Schema({
+    username: {type: String, required: true}
+}, {versionKey: false})
+
+let User = mongoose.model("User", userSchema)
+let Exercise = mongoose.model("Exercise", exerciseSchema)
+
+// DATABASE FUNCTIONS
+function createAndSaveUser(username, done) {
+    let user = new User({
+        username: username
+    })
+    user.save((err, data) => {
+        done(null, data)
+    })
+}
+
+function getAllUsers(done) {
+    User.find((err, data) => {
+        if (data) {
+            done(null, data)
+        }
+    })
+}
+
+exports.createUser = createAndSaveUser;
+exports.getAllUsers = getAllUsers
--- server.js
+++ server.js
@@ -1,69 +1,32 @@
-require('dotenv').config();
+
 const express = require("express");
 const app = express();
-let mongoose;
 
-try {
-  mongoose = require("mongoose");
-} catch (e) {
-  console.log(e);
-}
+const database = require("./database-functions.js")
 
+// BODY PARSER
 const bodyParser = require("body-parser");
-
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
 
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
-const { Schema } = mongoose;
-
-const userSchema = new Schema({
-    username: {type: String, required: true}
-})
-
-const exerciseSchema = new Schema({
-    username: {type: String, required: true}
-})
-
-let User = mongoose.model("User", userSchema)
-let Exercise = mongoose.model("Exercise", exerciseSchema)
-
-// FUNCTIONS FOR CRUD OPERATIONS. POTENTIALLY SEPERATE FROM BELOW ROUTES (ROUTER?)
-
-function createAndSaveUser(username, done) {
-    let user = new User({
-        username: username
-    })
-    user.save((err, data) => {
-        done(null, data)
-    })
-}
-
-function getAllUsers(done) {
-    
-}
-
-// POTENTIALLY SHOULD BE SEPERATED (THESE ARE THE ROUTES ACCESSED BY THE APP)
-
+// ROUTES
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
 
-app.get("/api/users", (req, res) => {
-
-})
-
-app.post("/api/users", (req, res, next) => {
-    console.log(req.body.username)
-    createAndSaveUser(req.body.username, (err, data) => {
+app.get("/api/users", (req, res, next) => {
+    database.getAllUsers((err, data) => {
         return next(data)
     })
 })
 
+app.post("/api/users", (req, res, next) => {
+    database.createUser(req.body.username, (err, data) => {
+        return next(data)
+    })
+})
+
+// LISTENER
 const listener = app.listen(process.env.PORT || 3000, function () {
     console.log("Your app is listening on port " + listener.address().port);
 });
